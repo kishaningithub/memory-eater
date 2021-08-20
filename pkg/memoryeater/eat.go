6ccd68178e716d099451acf243c742f1ff7d3c9e
Kishan B <bkishan@thoughtworks.com>
2021-08-20 18:23:23 +0530

    Use time.Ticker to fire eat operation at specified duration

--- pkg/memoryeater/eat.go
+++ pkg/memoryeater/eat.go
@@ -8,19 +8,17 @@ import (
 )
 
 func Eat(stepSize bytesize.ByteSize, stepDuration time.Duration) {
-	var memorySink [][]byte
-	for {
-		start := time.Now().UnixNano()
+	leak := make(map[int][]byte)
+	ticker := time.NewTicker(stepDuration)
+	i := 0
+	for range ticker.C {
 		stepSizeInBytes := int(stepSize)
 		bytes := make([]byte, stepSizeInBytes)
-		for i := 0; i < stepSizeInBytes; i++ {
-			bytes[i] = '2'
+		for j := 0; j < stepSizeInBytes; j++ {
+			bytes[j] = '2' // Using to move data from VSZ to RSS in order to eat up the RAM
 		}
-		memorySink = append(memorySink, bytes)
-		end := time.Now().UnixNano()
-		durationForMemoryIncrease := end - start
-		sleepDuration := time.Duration(int64(stepDuration) - durationForMemoryIncrease)
-		_, _ = fmt.Fprintf(os.Stderr, "\r Swallowed up %s from RAM (RSS Space)            ", bytesize.ByteSize(len(memorySink)*stepSizeInBytes))
-		time.Sleep(sleepDuration)
+		leak[i] = bytes
+		i++
+		_, _ = fmt.Fprintf(os.Stderr, "\r Swallowed up %s from RAM (RSS Space)            ", bytesize.ByteSize(len(leak)*stepSizeInBytes))
 	}
 }
